c55ad4f4819c9e90529800b2bf1a64e0081da40d
Arif Shaik <shaikarif00207@gmal.com>
2020-06-23 21:23:09 +0530

    deploy me react app to gith

---
+++ src/components/pages/Service.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h2>Service Page</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
