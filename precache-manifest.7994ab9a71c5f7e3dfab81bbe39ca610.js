912cf538b5a3f0f45b0ce2381dc62d1b6c08da61
Arif Shaik <shaikarif00207@gmal.com>
2020-06-24 20:27:27 +0530

    Updates

---
+++ precache-manifest.7994ab9a71c5f7e3dfab81bbe39ca610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00abb3cd788e96a110e2f0d535bf3258",
+    "url": "/portfolio_crud/index.html"
+  },
+  {
+    "revision": "54a3f85a964542f0c5e9",
+    "url": "/portfolio_crud/static/css/main.e917c0a8.chunk.css"
+  },
+  {
+    "revision": "b2d85166c45a83a5455f",
+    "url": "/portfolio_crud/static/js/2.4bff59cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_crud/static/js/2.4bff59cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a3f85a964542f0c5e9",
+    "url": "/portfolio_crud/static/js/main.52ed14d4.chunk.js"
+  },
+  {
+    "revision": "958290e98ae902145a3d",
+    "url": "/portfolio_crud/static/js/runtime-main.bd12f065.js"
+  }
+]);
